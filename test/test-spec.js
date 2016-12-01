19bfbc5604539cd921e260554afd84fd4d58a2e3
Forekid <only8623@hotmail.com>
2016-12-01 14:20:02 +0800

    add test

---
+++ test/test-spec.js
@@ -0,0 +1,13 @@
+var transform = require('../index.js').transform;
+
+describe('export-from-ie8', function () {
+  it('Object.defineProperty -> exports = ', function () {
+    expect(transform('Object.defineProperty(exports, "foo", { \
+      enumerable: true, \
+      get: function get() { \
+        return _baz.foo; \
+      } \
+    });'))
+      .toEqual('\nexports.foo = _baz.foo;');
+  });
+});
